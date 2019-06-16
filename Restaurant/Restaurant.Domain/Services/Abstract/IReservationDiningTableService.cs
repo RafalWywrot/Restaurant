402e462d4864f4e4b2bc0275e3e09a6ccb8e2019
wywpawel <wywpawel@gmail.com>
2019-06-16 23:05:57 +0200

    Add services to communicate with DB

---
+++ Restaurant/Restaurant.Domain/Services/Abstract/IReservationDiningTableService.cs
@@ -0,0 +1,15 @@
+﻿using Restaurant.Domain.DTO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Restaurant.Domain.Services.Abstract
+{
+    public interface IReservationDiningTableService
+    {
+        List<ReservationDiningTableDTO> GetReservations(string userId);
+        void CancelReservation(int reservationId);
+    }
+}
