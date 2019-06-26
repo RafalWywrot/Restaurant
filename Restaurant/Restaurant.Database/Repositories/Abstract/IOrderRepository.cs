df0e67355b9d37c270d7b2389865692a70f114a9
Rafal <Rafal.Wywrot@onet.pl>
2019-06-26 23:27:29 +0200

    add order repository

---
+++ Restaurant/Restaurant.Database/Repositories/Abstract/IOrderRepository.cs
@@ -0,0 +1,13 @@
+﻿using Restaurant.Database.Entities;
+using System.Collections.Generic;
+
+namespace Restaurant.Database.Repositories.Abstract
+{
+    public interface IOrderRepository
+    {
+        IList<Order> GetAll();
+        void InitializeOrder(Order order);
+        void AddOrderItem(OrderItem orderItem);
+        void Remove(int orderItemId);
+    }
+}
---
+++ Restaurant/Restaurant.Database/Repositories/Concrete/OrderRepository.cs
@@ -0,0 +1,79 @@
+﻿using NHibernate;
+using Restaurant.Database.Entities;
+using Restaurant.Database.Repositories.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Restaurant.Database.Repositories.Concrete
+{
+    public class OrderRepository : IOrderRepository
+    {
+        private NHibernate.ISessionFactory _sessionFactory { get; }
+
+        public OrderRepository(IRestaurantSessionFactory sessionFactory)
+        {
+            _sessionFactory = sessionFactory.CreateSessionFactory();
+        }
+
+        public IList<Order> GetAll()
+        {
+            using (ISession session = _sessionFactory.OpenSession())
+            {
+                return session.Query<Order>().ToList();
+            }
+        }
+
+        public void InitializeOrder(Order order)
+        {
+            using (ISession session = _sessionFactory.OpenSession())
+            {
+                using (var transaction = session.Transaction)
+                {
+                    transaction.Begin();
+                    session.Save(order);
+                    transaction.Commit();
+                }
+            }
+        }
+        public void AddOrderItem(OrderItem orderItem)
+        {
+            using (ISession session = _sessionFactory.OpenSession())
+            {
+                using (var transaction = session.Transaction)
+                {
+                    transaction.Begin();
+                    var orderDAO = session.Query<Order>().Where(x => x.Id == orderItem.OrderParent.Id).FirstOrDefault();
+                    orderDAO.OrderItems.Add(orderItem);
+                    session.Update(orderDAO);
+                    transaction.Commit();
+                }
+            }
+        }
+
+        public void Remove(int orderItemId)
+        {
+            using (ISession session = _sessionFactory.OpenSession())
+            {
+                using (var transaction = session.Transaction)
+                {
+                    transaction.Begin();
+                    var orderItemDAO = session.Query<OrderItem>().Where(x => x.Id == orderItemId).FirstOrDefault();
+                    var order = session.Query<Order>().Where(x => x.Id == orderItemDAO.OrderParent.Id).FirstOrDefault();
+                    if (order.OrderItems.Count > 1)
+                    {
+                        order.OrderItems.Remove(orderItemDAO);
+                        //session.Delete(orderItemDAO);
+                    }
+                    else
+                    {
+                        session.Delete(order);
+                    }
+                    transaction.Commit();
+                }
+            }
+        }
+    }
+}
