9df2b10a48f69f91a9cadf2790d6111c8093b9d4
wywpawel <wywpawel@gmail.com>
2019-06-29 23:13:33 +0200

    Add viewmodel for viewing history orders

---
+++ Restaurant/Restaurant.WebApplication/Models/OrdersViewModel.cs
@@ -0,0 +1,12 @@
+﻿using Restaurant.Domain.DTO;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace Restaurant.WebApplication.Models
+{
+    public class OrdersViewModel
+    {        
+        public OrderDTO ActiveOrder { get; set; }
+        public IList<OrderDTO> HistoryOrders { get; set; }
+    }
+}
