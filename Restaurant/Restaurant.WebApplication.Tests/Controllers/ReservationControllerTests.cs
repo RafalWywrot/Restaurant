e0f85a9df7014bb67f2e24df59e9eef0620832bb
Rafal <Rafal.Wywrot@onet.pl>
2019-06-30 02:33:49 +0200

    add unit tests

---
+++ Restaurant/Restaurant.WebApplication.Tests/Controllers/ReservationControllerTests.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Web.Mvc;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Restaurant.WebApplication.Controllers;
+namespace Restaurant.WebApplication.Tests.Controllers
+{
+    [TestClass]
+    public class ReservationControllerTests
+    {
+        [TestMethod]
+        public void Index()
+        {
+            
+        }
+        [TestMethod]
+        public void Index_should_return_error_when_exception()
+        {
+
+        }
+        [TestMethod]
+        public void Details()
+        {
+
+        }
+        [TestMethod]
+        public void Details_Post()
+        {
+
+        }
+        [TestMethod]
+        public void Create()
+        {
+
+        }
+        [TestMethod]
+        public void Create_Post()
+        {
+
+        }
+    }
+}
---
+++ Restaurant/Restaurant.WebApplication.Tests/Models/MenuViewModelTests.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Restaurant.WebApplication.Tests.Models
+{
+    [TestClass]
+    public class MenuViewModelTests
+    {
+        [TestMethod]
+        public void MenuViewModel_validation_should_return_invalid_when_name_option_no_selected()
+        {
+
+        }
+        [TestMethod]
+        public void MenuViewModel_validation_should_return_invalid_when_price_no_selected()
+        {
+
+        }
+        [TestMethod]
+        public void MenuViewModel_validation_should_return_true_when_modelState_is_valid()
+        {
+
+        }
+    }
+}
